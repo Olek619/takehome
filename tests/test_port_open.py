a50a9087bcdce14ec3e69dd3268f2a847a750060
olek619 <oleklevchenia@gmail.com>
2024-04-20 05:14:19 -0700

    added port

--- tests/test_port_open.py
+++ tests/test_port_open.py
@@ -1,49 +1,56 @@
-# from functions import is_port_open
-#
-#
-# def test_port_open():
-#     ip = "192.168.0.1"
-#     port = 80
-#     assert is_port_open(ip, port) == True
-#
-#
-# def test_port_closed():
-#     ip = "192.168.0.1"
-#     port = 81
-#     assert is_port_open(ip, port) == False
-#
-#
-# def test_port_unreachable():
-#     ip = "277.168.0.1"
-#     port = 80
-#     assert is_port_open(ip, port) == False
-#
-#
-# def test_negative_port_number():
-#     ip = "192.168.0.1"
-#     port = -1
-#     assert is_port_open(ip, port) == False
-#
-#
-# def test_large_port_number():
-#     ip = "192.168.0.1"
-#     port = 65536
-#     assert is_port_open(ip, port) == False
-#
-#
-# def test_non_integer_port_number():
-#     ip = "192.168.0.1"
-#     port = "not_a_port"
-#     assert is_port_open(ip, port) == False
-#
-#
-# def test_non_bool_port_number():
-#     ip = "192.168.0.1"
-#     port = True
-#     assert is_port_open(ip, port) == False
-#
-#
-# def test_non_float_port_number():
-#     ip = "192.168.0.1"
-#     port = 1.12
-#     assert is_port_open(ip, port) == False
+from functions import is_port_open
+
+
+def test_port_open():
+    ip = "www.google.com"
+    port = 80
+    assert is_port_open(ip, port) == True
+
+
+def test_port_closed():
+    ip = "www.google.com"
+    port = 81
+    assert is_port_open(ip, port) == False
+
+
+def test_port_unreachable():
+    ip = "nonexistent.example.com"
+    port = 80
+    assert is_port_open(ip, port) == False
+
+
+def test_port_incorret_ip():
+    ip = "257.168.0.1"
+    port = 80
+    assert is_port_open(ip, port) == False
+
+
+
+def test_negative_port_number():
+    ip = "www.google.com"
+    port = -1
+    assert is_port_open(ip, port) == False
+
+
+def test_large_port_number():
+    ip = "www.google.com"
+    port = 65536
+    assert is_port_open(ip, port) == False
+
+
+def test_non_integer_port_number():
+    ip = "www.google.com"
+    port = "not_a_port"
+    assert is_port_open(ip, port) == False
+
+
+def test_non_bool_port_number():
+    ip = "www.google.com"
+    port = True
+    assert is_port_open(ip, port) == False
+
+
+def test_non_float_port_number():
+    ip = "www.google.com"
+    port = 1.12
+    assert is_port_open(ip, port) == False
